86c36b05d70b9da43737a83beee984fdbf0f1888
Jozef Machac <84977782+machacjozef@users.noreply.github.com>
2024-04-11 17:40:11 +0200

    Initial commit

---
+++ src/env.template.js
@@ -0,0 +1,7 @@
+(function (window) {
+  window['env'] = window['env'] || {};
+
+  window['env']['services-log-level'] = "${LOG_LEVEL}"
+  window['env']['autoResolveBackendUrl'] = "${AUTO_RESOLVE_URL}"
+
+})(this)
